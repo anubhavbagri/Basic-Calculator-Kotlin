766c649a70b09459866bee28ecf29ddf6a0b3cf3
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-08-29 17:41:02 +0530

    feat: auto generate function definitions

--- app/src/main/java/com/example/basiccalculator/MainActivity.kt
+++ app/src/main/java/com/example/basiccalculator/MainActivity.kt
@@ -8,4 +8,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun allClearTap(view: android.view.View) {}
+    fun backspaceTap(view: android.view.View) {}
+    fun equalsTap(view: android.view.View) {}
 }
